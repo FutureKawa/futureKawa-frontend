03f002fbc18e63c00ef9e3033b464c552c78493f
PapaCheikh <papacheikhgningue@esp.sn>
2026-05-06 11:16:39 +0200

    feat: ajouter la gestion des alertes dans le composant Navbar avec un popup d'affichage

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,22 +1,36 @@
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, signal } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
 import { AlerteStore } from '../../core/stores/alerte.store';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DatePipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class NavbarComponent {
   private readonly alerteStore = inject(AlerteStore);
+  readonly isPopupOpen = signal<boolean>(false);
 
   readonly alertesCount = computed(() => {
     return this.alerteStore.alertesNonTraitees().length;
   });
 
+  readonly alertes = computed(() => {
+    return this.alerteStore.alertesNonTraitees();
+  });
+
   constructor() {
     // Charger toutes les alertes au démarrage
     this.alerteStore.loadAllAlertes();
+  }
+
+  togglePopup(): void {
+    this.isPopupOpen.update(val => !val);
+  }
+
+  closePopup(): void {
+    this.isPopupOpen.set(true);
   }
 }
